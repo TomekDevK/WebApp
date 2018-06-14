43ef7a98d4dd58b47c8a6b2559572a75b8f8b968
Tomek <tomasz.kaminski@pomelody.com>
2018-06-14 12:17:02 +0200

    Fun fun css

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,19 +7,19 @@ class Header extends Component {
     render() {
         return (
             <div className='header'>
-                <div className='home'>
+                <div className='home-header'>
                     <Link to='/'>Strona główna</Link>
                 </div>
-                <div className='goal'>
+                <div className='goal-header'>
                     <Link to='/celankiety'>Cel Ankiety</Link>
                 </div>
-                <div className='quest'>
+                <div className='quest-header'>
                     <Link to='/ankieta'>Ankieta</Link>
                 </div>
-                <div className='team'>
+                <div className='team-header'>
                     <Link to='/zespol'>Nasz zespół</Link>
                 </div>
-                <div className='source'>
+                <div className='source-header'>
                     <Link to='/zasoby'>Źródła i zasoby</Link>
                 </div>
             </div>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,64 @@
 // dependencies
 import React ,{Component} from 'react'
+import $ from 'jquery'
 
 class Home extends Component {
+    constructor () {
+        super ()
+        this.state = {
+            lFollowX: 0,
+            lFollowY: 0,
+            x: 0,
+            y: 0,
+            friction: 1 /10
+        }
+    }
+
+    componentDidMount () {
+        let that = this
+        $(window).on('mousemove click', function(e) {
+
+            var lMouseX = Math.max(-300, Math.min(300, $(window).width() / 2 - e.clientX));
+            var lMouseY = Math.max(-300, Math.min(300, $(window).height() / 2 - e.clientY));
+            var lFollowX = (lMouseX) ; // 100 : 12 = lMouxeX : lFollow
+            var lFollowY = (lMouseY) ;
+
+            that.setState({
+                lFollowX: lFollowX,
+                lFollowY: lFollowY
+            })
     
+        });
+    }
+
+    moveBackground = () => {
+    let {x,y,lFollowX,lFollowY,friction} = this.state
+
+    x += (lFollowX - x) * friction
+    y += (lFollowY - y) * friction
+    
+    var translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+
+    $('.bg').css({
+        '-webit-transform': translate,
+        '-moz-transform': translate,
+        'transform': translate
+    });
+
+    window.requestAnimationFrame(this.moveBackground);
+    }
+
+   
 
 
     render() {
+        this.moveBackground()
         return (
             <div className='home'>
+                <div className="wrap">
+                    <div className="bg"></div>
+                    <h1>Wypełnij ankietę</h1>
+                </div>
             </div>
         )
     }
